3997d220c9c4b7cb88ddd266cbb5d34ab398448d
Felipe Scarel <fbscarel@gmail.com>
2018-08-05 17:32:25 -0300

    pscount.sh for per-user process counting

---
+++ seg12/semana_1/sessao_3/pscount.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+users=( $( ps aux | awk {'print $1'} | grep -v USER | sort | uniq ) )
+
+for (( i=0; i<${#users[@]}; i++ )); do
+  nproc=$( ps aux | grep "${users[$i]}" | wc -l )
+  echo -e "User ${users[$i]} has $nproc active processes"
+done
