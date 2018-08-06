44bb3ed67c6794f2072838403d59c5d1e1e4fd3b
Felipe Scarel <fbscarel@gmail.com>
2018-08-06 18:23:14 -0300

    remove comments, save for classroom

--- seg12/semana_1/sessao_4/edquota_batch.py
+++ seg12/semana_1/sessao_4/edquota_batch.py
@@ -22,35 +22,27 @@ done
 
 import os, sys, subprocess, pwd, spwd
 
-# check perms
 if os.geteuid() != 0:
   exit('  Not root?')
 
-# check parameters
 if len(sys.argv) <= 1:
   exit('  Usage: ' + sys.argv[0] + ' TEMPLATE_USER')
 
-# check if user exists
 try:
   pwd.getpwnam(sys.argv[1])
 except KeyError:
   exit('No such \'' + sys.argv[1] + '\' user')
 
-# initialize empty list
 qusers = []
 
 for user in pwd.getpwall():
-  # remove bogus users
   if user[0] == 'root' or user[0] == sys.argv[1]:
     continue
 
-  # get password hash
   phash = spwd.getspnam(user[0]).sp_pwd
 
-  # test for system users
   if phash != '*' and not phash.startswith('!'):
     qusers.append(user[0])
 
-# set quotas according to template
 for user in qusers:
   subprocess.call(['edquota', '-u', user, '-p', sys.argv[1]])
