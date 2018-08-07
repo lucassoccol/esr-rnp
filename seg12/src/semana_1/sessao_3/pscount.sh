acb3a4e2fe4e86272438c64f0029d9c2e94b880e
Felipe Scarel <fbscarel@gmail.com>
2018-08-07 16:54:51 -0300

    remove first line from ps in a more fashionable way

--- seg12/src/semana_1/sessao_3/pscount.sh
+++ seg12/src/semana_1/sessao_3/pscount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-users=( $( ps aux | awk {'print $1'} | grep -v USER | sort | uniq ) )
+users=( $( ps aux | awk '{ if (NR>1) print $1 }' | sort | uniq ) )
 
 for (( i=0; i<${#users[@]}; i++ )); do
   nproc=$( ps aux | grep "${users[$i]}" | wc -l )
