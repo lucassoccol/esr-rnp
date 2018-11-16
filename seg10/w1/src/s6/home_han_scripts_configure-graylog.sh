de441d4e94e6e44d75b6687d9e7ea3e97099e48b
Felipe Scarel <fbscarel@gmail.com>
2018-11-16 03:55:09 -0200

    seg10/s6 done

--- seg10/w1/src/s6/home_han_scripts_configure-graylog.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo '*.* @10.0.42.4:5140;RSYSLOG_SyslogProtocol23Format' > /etc/rsyslog.d/90-graylog.conf
-systemctl restart rsyslog.service
--- seg10/w1/src/s6/home_han_scripts_install-snoopy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# instalacao
-DEBIAN_FRONTEND=noninteractive apt-get -yq install snoopy
-
-# configuracao
-echo "/lib/snoopy.so" > /etc/ld.so.preload
